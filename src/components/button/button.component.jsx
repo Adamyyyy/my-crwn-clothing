f5154bfd26ecb76e804bbfdc4e768b76d84e9c91
Adamyyyy <happyadam2008@gmail.com>
2023-01-21 10:35:12 +1100

    add payment component

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,4 +1,9 @@
-import { BaseButton, GoogleSignButton, InvertButton } from "./button.styles";
+import {
+  BaseButton,
+  GoogleSignButton,
+  InvertButton,
+  ButtonSpinner,
+} from "./button.styles";
 
 export const BUTTON_TYPE_CLASSES = {
   base: "base",
@@ -13,9 +18,13 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
     [BUTTON_TYPE_CLASSES.inverted]: InvertButton,
   }[buttonType]);
 
-const Button = ({ children, buttonType, ...otherProps }) => {
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
   const CustomButton = getButton(buttonType);
-  return <CustomButton {...otherProps}>{children}</CustomButton>;
+  return (
+    <CustomButton disabled={isLoading} {...otherProps}>
+      {isLoading ? <ButtonSpinner /> : children}
+    </CustomButton>
+  );
 };
 
 export default Button;
